7be1bbeef11bb1b74e4cdd787835f359a11e8e88
GitSpecro <acvlok01@gmail.com>
2025-03-11 20:00:03 +0200

    Add Hero Section

--- scripts/main.js
+++ scripts/main.js
@@ -12,3 +12,17 @@ document.querySelectorAll('#nav-links a').forEach(link => {
         navLinks.classList.remove('active');
     });
 });
+
+// Image Slider
+const slides = document.querySelector('#slides');
+let index = 0;
+
+function nextSlide() {
+    index++;
+    if (index >= slides.children.length) {
+        index = 0; // Reset to first image
+    }
+    slides.style.transform = `translateX(-${index * 100}%)`;
+}
+
+setInterval(nextSlide, 2000); // Change image every 2 seconds
